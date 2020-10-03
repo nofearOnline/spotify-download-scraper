937d2c1897dd8ff8b996b799d2aef70900daa32b
Gil Rubin <gilrub0@gmail.com>
2020-10-03 20:52:38 +0300

    ffmpeg installer by pep8 standard

--- installer.py
+++ installer.py
@@ -1,9 +1,8 @@
 import os
 import platform
-
+from zipfile import ZipFile
 if "Windows" in platform.platform():
     import urllib.request
-from zipfile import ZipFile
 
 
 def check_if_in_path():
@@ -13,17 +12,16 @@ def check_if_in_path():
     :return:
     """
     path = os.getenv('PATH')
-    if "Windows" in platform.platform():
-        if "ffmpeg" not in path:
-            print("not in path, adding it to path")
-            os.system('SETX PATH "%PATH%;{0}" '.format(r"C:\ffmpeg\bin"))
-        else:
-            print("already in path")
+    if "ffmpeg" not in path:
+        print("not in path, adding it to path")
+        os.system('SETX PATH "%PATH%;{0}" '.format(r"C:\ffmpeg\bin"))
+    else:
+        print("already in path")
 
 
 def check_if_downloaded() :
     """
-    check if the files required are downloaded and if not it downloading and unzipping.
+    check if the files required are downloaded and if not it downloading and preparing it.
 
     :return:
     """
@@ -46,27 +44,28 @@ def check_if_downloaded() :
                     break
         else:
             print("already downloaded")
-
-    if "Linux" in platform.platform():
+    elif "Linux" in platform.platform():
         pm = find_package_manager()
         if pm == "apt":
-            os.system(
-                r'sudo add-apt-repository ppa:mc3man/trusty-media; sudo apt-get update; sudo apt-get install -y ffmpeg; ')
-        if pm == "deb":
+            os.system(r'sudo add-apt-repository ppa:mc3man/trusty-media; sudo apt-get update \
+            sudo apt-get install -y ffmpeg;')
+        elif pm == "deb":
             dist = input("enter your distribution: (‘stretch‘, ‘jessie‘, or ‘wheezy‘)")
             os.system(
-                r'echo deb http://www.deb-multimedia.org {0} main non-free deb-src http://www.deb-multimedia.org {0} main non-free >> /etc/apt/sources.list'.format(
-                    dist))
-            os.system(
-                r'sudo apt-get update; sudo apt-get install -y deb-multimedia-keyring; sudo apt-get update;  sudo apt-get install -y ffmpeg')
-        if pm == "yum":
-            os.system(
-                r'yum install -y epel-release; yum localinstall -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm')
+                r'echo deb http://www.deb-multimedia.org {0} main non-free deb-src http://www.deb-multimedia.org {0} \
+                main non-free >> /etc/apt/sources.list'.format(dist))
+            os.system(r'sudo apt-get update; sudo apt-get install -y deb-multimedia-keyring; sudo apt-get update;  \
+            sudo apt-get install -y ffmpeg')
+        elif pm == "yum":
+            os.system(r'yum install -y epel-release; yum localinstall -y --nogpgcheck \
+            https://download1.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm \
+            https://download1.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm')
             os.system(r'yum install -y ffmpeg ffmpeg-devel')
-        if pm == 'dnf':
-            os.system(
-                r'sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm; sudo dnf install -y ffmpeg ffmpeg-devel')
-    if "macos" in platform.platform().lower():
+        elif pm == 'dnf':
+            os.system(r'sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm \
+            -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E \
+            %fedora).noarch.rpm; sudo dnf install -y ffmpeg ffmpeg-devel')
+    elif "macos" in platform.platform().lower():
         os.system(r'brew install ffmpeg')
 
 
@@ -74,11 +73,11 @@ def find_package_manager():
     usr_bin = os.listdir('/usr/bin')
     if "yum" in usr_bin:
         return "yum"
-    if "apt" in usr_bin:
+    elif "apt" in usr_bin:
         return "apt"
-    if "deb" in usr_bin:
+    elif "deb" in usr_bin:
         return "deb"
-    if "dnf" in usr_bin:
+    elif "dnf" in usr_bin:
         return "dnf"
 
 
@@ -86,8 +85,8 @@ def install_deps():
     os.system(r"pip3 install -r requirements.txt")
 
 
-
 check_if_downloaded()
-check_if_in_path()
 install_deps()
+if "Windows" in platform.platform():
+    check_if_in_path()
 print("INSTALLED - enjoy")
